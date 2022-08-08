0e33c5a0f1d630b881651cf7baee7cc3b51fce1d
Daniel Johnson <danielrjohnson00@gmail.com>
2022-08-07 20:40:20 -0500

    add base stack implementation and change main and VM to empty

---
+++ VM/stack/stack_test.go
@@ -0,0 +1,59 @@
+package stack
+
+import "testing"
+
+func Test_New_ReturnsEmptyStack(t *testing.T) {
+	s := New[int]()
+	if !s.IsEmpty() {
+		t.Errorf("New stack is not empty. Size Expected=%d, Got=%d", 0, len(s.items))
+	}
+}
+
+func Test_Push_IncrementsSize(t *testing.T) {
+	s := New[int]()
+	s.Push(1)
+	if s.Size() != 1 {
+		t.Errorf("Push did not increment size. Size Expected=%d, Got=%d", 1, s.Size())
+	}
+}
+
+func Test_Pop_DecrementsSize(t *testing.T) {
+	s := New[int]()
+	s.Push(1)
+	s.Pop()
+	if s.Size() != 0 {
+		t.Errorf("Pop did not decrement size. Size Expected=%d, Got=%d", 0, s.Size())
+	}
+}
+
+func Test_Pop_ReturnsCorrectItem(t *testing.T) {
+	s := New[int]()
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+	item := s.Pop()
+	if item != 3 {
+		t.Errorf("Pop did not return correct item. Item Expected=%d, Got=%d", 3, item)
+	}
+}
+
+func Test_Pop2_DecrementsSizeBy2(t *testing.T) {
+	s := New[int]()
+	s.Push(1)
+	s.Push(2)
+	s.Pop2()
+	if s.Size() != 0 {
+		t.Errorf("Pop2 did not decrement size by 2. Size Expected=%d, Got=%d", 0, s.Size())
+	}
+}
+
+func Test_Pop2_ReturnsCorrectItems(t *testing.T) {
+	s := New[int]()
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+	item1, item2 := s.Pop2()
+	if item1 != 3 || item2 != 2 {
+		t.Errorf("Pop2 did not return correct items. Items Expected=(%d, %d), Got=(%d, %d)", 3, 2, item1, item2)
+	}
+}
--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/danielrjohnson/glVM/VM"
 )
 
 func main() {
-	fmt.Println(VM.HelloWorld)
+	fmt.Println("Hello VM")
 }
