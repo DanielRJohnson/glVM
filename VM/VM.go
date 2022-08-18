b0bf39ec9fed2af8b194586e8ec67b7d0d1806cb
Daniel Johnson <danielrjohnson00@gmail.com>
2022-08-17 20:41:56 -0500

    add VM struct

--- VM/VM.go
+++ VM/VM.go
@@ -1,1 +1,35 @@
 package VM
+
+import (
+	"fmt"
+
+	"github.com/danielrjohnson/glVM/VM/stack"
+	"github.com/danielrjohnson/glVM/instructions"
+	"github.com/danielrjohnson/glVM/program"
+)
+
+type VM[T any] struct {
+	program program.Program[T]
+	ip      uint64
+	stack   stack.Stack[T]
+}
+
+func New[T any](program program.Program[T]) VM[T] {
+	return VM[T]{
+		program,
+		0,
+		stack.New[T](),
+	}
+}
+
+func (vm VM[T]) Run() {
+	code := vm.program.Code()
+	for int(vm.ip) < len(code) {
+		instr := code[vm.ip]
+		switch instr {
+		case instructions.NOOP:
+			fmt.Println("NOOP :)")
+		}
+		vm.ip++
+	}
+}
